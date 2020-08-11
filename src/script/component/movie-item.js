fa9d9e36024310d66ee85c9f22e5f704fa4ba342
13513060 <13513060@std.stei.itb.ac.id>
2020-08-12 03:07:55 +0700

    add custome element to display movies

--- src/script/component/movie-item.js
+++ src/script/component/movie-item.js
@@ -20,25 +20,33 @@ class MovieItem extends HTMLElement {
             .tmdb-logo {
                 width: 200px;
             }
+            @media screen and (max-width: 360px) {
+                .movie-info {
+                    min-width: 240px;
+                }
+                .media-body h3 {
+                    font-size: 1.4em;
+                }
+            }
 		</style>
-		<section class="movie media flex-wrap shadow-lg rounded p-4">
-            <img class="mr-3" src="https://raw.githubusercontent.com/nursyahrina/MovieReviews/master/assets/nursyahrina.jpg" alt="Card image cap" style="width: 200px;">
-            <div class="media-body d-flex flex-column align-items-stretch ">
-                <h3 class="p-2 mt-3">Movie title</h3>
+		<section class="movie media flex-wrap shadow rounded p-4 mb-3">
+            <img class="my-3 mr-2 rounded" src="https://image.tmdb.org/t/p/w200${this._movie.poster_path}" alt="${this._movie.title} Movie Poster" style="width: 200px;">
+            <div class="media-body d-flex flex-column align-items-stretch m-2">
+                <h3 class="text-primary p-2">${this._movie.title}</h3>
                 <div class="d-flex flex-wrap">
-                    <p class="px-2 mb-2 text-muted movie-info">Release date: Wed July 12 2019</p>
-                    <p class="px-2 mb-2 text-muted movie-info">Genres: </p>
+                    <p class="px-2 text-muted movie-info">Release date: ${new Date(this._movie.release_date).toDateString()}</p>
+                    <p class="px-2 text-muted movie-info">Genres: </p>
                 </div>
-                <p class="p-2">Movie desciption: Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="p-2"><em>${this._movie.overview}<em></p>
                 <div class="d-flex flex-row justify-content-between">
-                    <p class="rating mb-2 px-2"> Rating:<br>
+                    <p class="rating p-2"> Rating:<br>
                         <i class="mt-2 fas fa-star fa-lg"></i>
                         <i class="fas fa-star fa-lg"></i>
                         <i class="fas fa-star fa-lg"></i>
                         <i class="fas fa-star-half-alt fa-lg"></i>
                         <i class="far fa-star fa-lg"></i>
                     </p>
-                    <a href="#" class="btn btn-primary align-self-end m-2 px-3">Reviews<i class="fas fa-angle-double-down ml-2"></i></a>
+                    <a href="#" class="btn btn-primary align-self-end px-3">Reviews<i class="fas fa-angle-double-down ml-2"></i></a>
                 </div>
             </div>
         </section>
---
+++ src/script/component/movie-list.js
@@ -0,0 +1,24 @@
+import './movie-item.js';
+
+class MovieList extends HTMLElement {
+    set movies(movies) {
+        this._movies = movies;
+        this.render();
+    }
+
+    renderError(message) {
+        this.innerHTML = `<h2 class="mt-4 ml-4 display-4">Movies</h2>`;
+        this.innerHTML += `<h2 class="lead text-muted mt-3 ml-4">${message}</h2>`;
+    }
+
+    render() {
+        this.innerHTML = `<h2 class="mb-3 ml-4 display-4">Movies</h2>`;
+        this._movies.forEach(movie => {
+            const movieItemElement = document.createElement("movie-item");
+            movieItemElement.movie = movie
+            this.appendChild(movieItemElement);
+        })
+    }
+}
+
+customElements.define("movie-list", MovieList);
