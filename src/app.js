c8c564e46cb2f6813544b74d790b1cba5302c4b0
13513060 <13513060@std.stei.itb.ac.id>
2020-08-11 13:57:58 +0700

    fontawesome using webpack dependency

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+import 'regenerator-runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import './script/component/nav-bar.js';
 import './script/component/search-header.js';
 import './script/component/aboutapp-article.js';
 import './script/component/app-footer.js';
-import './script/component/movie-item.js';
 import './styles/style.css';
 import main from './script/view/main.js';
  
