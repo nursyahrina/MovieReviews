4088a9756882905c05685793ab5392de9eae85b5
13513060 <13513060@std.stei.itb.ac.id>
2020-08-13 23:30:08 +0700

    build with complete features

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 
 module.exports = merge(common, {
@@ -20,5 +21,10 @@ module.exports = merge(common, {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new MinifyPlugin({}, {
+            comments: false
+        })
+    ]
 })
